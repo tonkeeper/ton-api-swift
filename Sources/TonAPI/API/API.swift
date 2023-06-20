d2dab30a137ee3df98f8997a552d11c5fb2dad05
Grigory Serebryanyy <grishamsc@gmail.com>
2023-06-20 10:18:22 +0200

    API Layer
    - remove Version

--- Sources/TonAPI/API/API.swift
+++ Sources/TonAPI/API/API.swift
@@ -8,23 +8,15 @@
 import Foundation
 
 public final class API {
-  
-  public enum Version {
-    case v2
-  }
-  
   let transport: NetworkTransport
   let baseURL: URL
-  let version: Version
   let responseDecoder: APIResponseDecoder
   
   public init(transport: NetworkTransport,
               baseURL: URL,
-              version: Version,
               responseDecoder: APIResponseDecoder) {
     self.transport = transport
     self.baseURL = baseURL
-    self.version = version
     self.responseDecoder = responseDecoder
   }
   

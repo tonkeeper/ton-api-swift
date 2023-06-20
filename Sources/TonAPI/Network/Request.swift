16e3372ff24c35a50639ef2ccb418dd6e127020f
Grigory Serebryanyy <grishamsc@gmail.com>
2023-06-20 10:18:58 +0200

    Network layer
    - add public init to Request
    - remove baseURL from URLSessionTransport
    - update tests

--- Sources/TonAPI/Network/Request.swift
+++ Sources/TonAPI/Network/Request.swift
@@ -13,4 +13,16 @@ public struct Request {
   var headers: [HTTPHeader]
   let queryItems: [URLQueryItem]
   let bodyParameter: HTTPParameters
+    
+  public init(path: String,
+              method: HTTPMethod,
+              headers: [HTTPHeader],
+              queryItems: [URLQueryItem],
+              bodyParameter: HTTPParameters) {
+    self.path = path
+    self.method = method
+    self.headers = headers
+    self.queryItems = queryItems
+    self.bodyParameter = bodyParameter
+  }
 }

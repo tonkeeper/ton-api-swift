3252791d91e0d4c1a6278a1e60d0f83137eea37c
Grigory Serebryanyy <grishamsc@gmail.com>
2023-10-27 20:01:13 +0200

    EventSource
    - fix cancellation issue in EventSource
    - remove eventSource with generic entity method

--- Packages/EventSource/Sources/EventSource.swift
+++ Packages/EventSource/Sources/EventSource.swift
@@ -11,20 +11,20 @@ public protocol APIError: Swift.Error & Decodable {}
 
 public struct EventSource {
   public struct Event {
-    let id: String?
-    let event: String?
-    let data: String?
+    public let id: String?
+    public let event: String?
+    public let data: String?
   }
   
   public static func eventSource<Sequence: AsyncSequence>(
     _ closure: () async throws -> Sequence,
     errorParser: EventSourceErrorParser? = nil
-  ) async throws -> (AsyncThrowingStream<Event, Swift.Error>)
+  ) async throws -> (AsyncThrowingStream<[Event], Swift.Error>)
   where Sequence.Element == ArraySlice<UInt8> {
     let sequence = try await closure()
     try Task.checkCancellation()
-    return AsyncThrowingStream<Event, Swift.Error> { continuation in
-      Task {
+    return AsyncThrowingStream<[Event], Swift.Error> { continuation in
+      let task = Task {
         var parser = EventParser()
         var errorParser = errorParser
         do {
@@ -33,46 +33,16 @@ public struct EventSource {
             try errorParser?.extractError()
             parser.append(bytes: Array(bytes))
             let events = parser.extractEvents()
-            events.forEach { continuation.yield($0) }
+            continuation.yield( events )
           }
           continuation.finish()
         } catch {
           continuation.finish(throwing: error)
         }
       }
-    }
-  }
-  
-  public static func eventSource<Sequence: AsyncSequence, Entity: Decodable>(
-    _ closure: () async throws -> Sequence,
-    errorParser: EventSourceErrorParser? = nil
-  ) async throws -> (AsyncThrowingStream<Entity, Swift.Error>)
-  where Sequence.Element == ArraySlice<UInt8> {
-    let eventStream = try await eventSource(
-      closure,
-      errorParser: errorParser
-    )
-    return AsyncThrowingStream<Entity, Swift.Error> { continuation in
-      Task {
-        let decoder = EventSourceMessageDecoder(jsonDecoder: JSONDecoder())
-        do {
-          for try await event in eventStream {
-            switch (event.event, event.data) {
-            case ("message", .some(let dataString)):
-              guard let model: Entity = try? decoder.decodeEventData(dataString) else {
-                continue
-              }
-              continuation.yield(model)
-            default: continue
-            }
-          }
-          continuation.finish()
-        } catch let error {
-          continuation.finish(throwing: error)
-        }
+      continuation.onTermination = { _ in
+        task.cancel()
       }
     }
   }
 }
-
-
